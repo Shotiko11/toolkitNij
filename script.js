0e3898a016429830aaf9ee24ae3a09025f04799b
Shotiko11 <shotikokokilashvili9@gmail.com>
2023-09-01 17:01:27 +0400

    third commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   const showButtonsButton = document.getElementById('showButtons');
   const overlay = document.getElementById('overlay');
-  const closeOverlayButton = document.getElementById('closeOverlay'); 
+  const closeOverlayButton = document.getElementById('closeOverlay');
 
   let buttonsVisible = false;
 
@@ -23,10 +23,44 @@ document.addEventListener('DOMContentLoaded', function () {
     buttonContainer.appendChild(document.createTextNode('\u00A0\u00A0'));
     buttonContainer.appendChild(button2);
 
-    buttonContainer.style.marginTop = '600px'; 
+    buttonContainer.style.marginTop = '600px';
     buttonContainer.style.marginLeft = '100px';
-    
+
     overlay.appendChild(buttonContainer);
+
+    const combinedElement = document.createElement('div');
+    combinedElement.classList.add('combined-element'); 
+    combinedElement.style.position = 'absolute';
+    combinedElement.style.zIndex = '99999';
+    combinedElement.style.top = '65px'; 
+    combinedElement.style.left = 'calc(50% - 218px)'; 
+
+    const rectangle = document.createElement('div');
+    rectangle.style.width = '220px';
+    rectangle.style.height = '33px';
+    rectangle.style.backgroundColor = '#F1EEDF';
+    rectangle.style.borderRadius = '10px';
+    rectangle.style.textAlign = 'center'; 
+    rectangle.style.lineHeight = '33px'; 
+    rectangle.style.color = 'brown'; 
+    rectangle.textContent = 'This is home page'; 
+
+   
+    const triangle = document.createElement('div');
+    triangle.style.width = '0';
+    triangle.style.height = '0';
+    triangle.style.borderLeft = '10px solid transparent';
+    triangle.style.borderRight = '10px solid transparent';
+    triangle.style.borderBottom = '10px solid #F1EEDF';
+    triangle.style.position = 'absolute';
+    triangle.style.top = '-10px'; 
+    triangle.style.left = 'calc(50% - 10px)';
+
+    rectangle.appendChild(triangle);
+
+    combinedElement.appendChild(rectangle);
+
+    overlay.appendChild(combinedElement);
   }
 
   function hideOverlay() {
@@ -37,20 +71,25 @@ document.addEventListener('DOMContentLoaded', function () {
       overlay.removeChild(buttonContainer);
     }
 
-    buttonsVisible = false; // Reset buttonsVisible to false when the overlay is closed
+    const combinedElement = overlay.querySelector('.combined-element');
+    if (combinedElement) {
+      overlay.removeChild(combinedElement);
+    }
+
+    buttonsVisible = false;
   }
 
   showButtonsButton.addEventListener('click', function () {
     if (buttonsVisible) {
-      hideOverlay(); 
+      hideOverlay();
     } else {
-      showOverlay(); 
+      showOverlay();
     }
 
     buttonsVisible = !buttonsVisible;
   });
 
   closeOverlayButton.addEventListener('click', function () {
-    hideOverlay(); 
+    hideOverlay();
   });
 });
